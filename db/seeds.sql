48451d5473d0857c24cfb5153e65f9fe0f2f270a
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-07-28 23:07:46 -0700

    allFiles

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name)
+VALUES ('Double Double'),
+       ('Ultimate Bacon Burger'),
+       ('Animal Style Western Meat Lover 3x Patty'),
+       ('Protein Style');
+
+
+SELECT * FROM burgers;
